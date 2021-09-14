336f624d97b25ae551a55baf921c53331dd382e6
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-09-14 16:23:13 -0300

    Challenge finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,21 @@
-export function Content() {
-  // Complete aqui
+import { ReactNode } from 'react'
+
+type ContentProps = {
+  children: ReactNode;
+  title: string;
+}
+
+export function Content({ children, title }: ContentProps) {
+  return (
+    <div className="container">
+      <main>
+      <header>
+            <span className="category">Categoria:<span> {title}</span></span>
+        </header>
+        <div className="movies-list">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,17 @@
-export function SideBar() {
-  // Complete aqui
+import { ReactNode } from "react"
+
+type SidebarProps = {
+  children: ReactNode;
+}
+
+export function SideBar({ children }: SidebarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {children}
+      </div>
+    </nav>
+  )
 }
